d90030a8627b839a3a8d66ef2e4cb7637e58a6a5
inkvi <alex@inkvi.com>
2025-01-09 15:09:33 -0800

    Add debugging information

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { KMSProviderGCP } from "./KMSProviderGCP";
 import { KMSWallets } from "./web3-kms-signer/kms-wallets";
 import { Signer } from "./web3-kms-signer/core";
 import { loadKZG } from "kzg-wasm";
+import * as console from "node:console";
 
 
 const app = fastify({
@@ -61,8 +62,14 @@ app.post('/', async (request, reply) => {
     case 'health_status':
       return reply.code(200).send({result: 'ok'});
     case 'sign':
-        const signature = await wallets.ecsign({keyId: keyId}, Buffer.from(params[0].slice(2), 'hex'), 1);
-        const res = Buffer.concat([signature.r, signature.s, bigintToBuffer(signature.v)]).toString('hex');
+      let digest = Buffer.from(params[0].slice(2), 'hex');
+      console.log('Digest:', digest.toString('hex'));
+      const signature = await wallets.ecsign({keyId: keyId}, digest, 1);
+        console.log('Signature r, s, v:', signature.r.toString('hex'), signature.s.toString('hex'), signature.v.toString(16));
+      const rPadded = Buffer.concat([Buffer.alloc(32 - signature.r.length, 0), signature.r]);
+      const sPadded = Buffer.concat([Buffer.alloc(32 - signature.s.length, 0), signature.s]);
+      const res = Buffer.concat([rPadded, sPadded, bigintToBuffer(signature.v)]).toString('hex');
+        console.log('Signature:', res);
       return reply.code(200).send({result: res});
     default:
       reply.code(400).send({error: 'Method not supported'});
